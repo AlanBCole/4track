c9e47688da2dd22a5a0bab473e3329cc8e5725bd
Alan Cole <alantuneit@gmail.com>
2019-11-21 19:13:28 -0700

    play position is updated in the UI. Still need to be able to set play position to the time in the transport input

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,24 @@
 import Transport from './models/Transport.js';
 
 let context = new AudioContext();
+
+// TRANSPORT
 const transport = new Transport(context);
 
+const transportContainer = document.querySelector('#transport');
 const startButton = document.querySelector('#play');
 const stopButton = document.querySelector('#stop');
+const transportDisplay = document.querySelector('#songPosition');
+transportContainer.addEventListener('headMove', (event) => event.target.value = event.detail.time.toFixed(2));
 startButton.addEventListener('click', () => startOrStop('start'));
 stopButton.addEventListener('click', () => startOrStop('stop'));
+
 
 function startOrStop (startorstop) {
     if (context.state === 'suspended') context.resume();
     
     if (startorstop === 'start') {
-        transport.start();
+        transport.play(songPosition);
         startButton.disabled = true;
     }
     else if (startorstop === 'stop') {
@@ -21,26 +27,28 @@ function startOrStop (startorstop) {
     }
 }
 
-const mixer = document.querySelector('#mixer');
-mixer.addEventListener('click', (e) => {
-    const [ start, stop, audio ] = e.target.parentNode.children;
-    const id = e.target.parentNode.id;
+// MIXER
+
+// const mixer = document.querySelector('#mixer');
+// mixer.addEventListener('click', (e) => {
+//     const [ start, stop, audio ] = e.target.parentNode.children;
+//     const id = e.target.parentNode.id;
     
-    if (e.target === start) {
-        console.log(`clicked track-${id}`, start);
-        stop.disabled = false;
-        start.disabled = true;
+//     if (e.target === start) {
+//         console.log(`clicked track-${id}`, start);
+//         stop.disabled = false;
+//         start.disabled = true;
         
-        navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-        .then((stream) => startRecording(stream, stop, audio));
-    }
+//         navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+//         .then((stream) => startRecording(stream, stop, audio));
+//     }
     
-    else if (e.target === stop) {
-        console.log(`clicked track-${id}`, stop);
-        stop.disabled = true;
-        start.disabled = false;
-    }
-});
+//     else if (e.target === stop) {
+//         console.log(`clicked track-${id}`, stop);
+//         stop.disabled = true;
+//         start.disabled = false;
+//     }
+// });
 
 // start.onclick = (e) => {
 //     start.disabled = true;
--- src/scripts/models/Transport.js
+++ src/scripts/models/Transport.js
@@ -1,21 +1,24 @@
 class Transport {
-    constructor(audiocontext) {
+    constructor(audiocontext, transportDisplayHtml) {
         this.startTime = 0;
         this.headFrame = 0;
         this.startFrame = 0;
         this.stopFrame = 0;
-        this.length = 0;
-        this.isStarted = false;
+        // this.length = 0;
+        this.isPlaying = false;
         this.isRecording = false;
         this.context = audiocontext;
+        this.playInterval = null;
+        // this.transportDisplay = transportDisplayHtmlElement
     }
 
-    start() {
-        if (!this.isStarted) {
-            this.isStarted = true;
+    play(htmlTransportDisplayElement) {
+        if (!this.isPlaying) {
+            this.isPlaying = true;
             this.startTime = this.context.currentTime;
             this.startFrame = this.headFrame;
             console.log(`starting playback at ${this.startFrame / this.context.sampleRate}.`);
+            this.start(htmlTransportDisplayElement);
         }
         else return;
     }
@@ -23,8 +26,9 @@ class Transport {
     stop() {
         if (this.isRecording) this.isRecording = false;
         
-        if (this.isStarted) {
-            this.isStarted = false;
+        if (this.isPlaying) {
+            clearInterval(this.playInterval);
+            this.isPlaying = false;
             let stopTime = this.context.currentTime;
             this.stopFrame = parseInt(((stopTime - this.startTime) * this.context.sampleRate)) + this.startFrame;
             this.headFrame = this.stopFrame;
@@ -34,6 +38,27 @@ class Transport {
             
         }
         else console.log('Please press play first!');
+    }
+    
+    start(htmlElement) {
+        if (this.isPlaying) {
+            this.playInterval = setInterval(() => {
+                let now = this.context.currentTime;
+                this.headFrame = parseInt(((now - this.startTime) * this.context.sampleRate)) + this.startFrame;
+                this.emitHeadPosition(htmlElement);
+            },
+            10);
+        }
+    }
+    
+    // where is the best place to pass in the htmlElement when invoking this command
+    emitHeadPosition(htmlElement) {
+        let event = new CustomEvent('headMove', { bubbles: true, detail: { time: this.headFrame / this.context.sampleRate }});
+        htmlElement.dispatchEvent(event);
+    }
+    
+    set headPosition(time) {
+        this.headPosition = parseInt(time * this.context.sampleRate);
     }
 }
 
